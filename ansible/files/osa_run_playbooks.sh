f5bbbb064f91128cb23a2e48ca361f262f0108ab
Olivier Bourdon <obourdon@opennext.io>
2018-04-16 21:36:47 +0200

    Add final OSA deployment script

--- ansible/files/osa_run_playbooks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /opt/openstack-ansible/playbooks
-openstack-ansible setup-hosts.yml
-openstack-ansible setup-infrastructure.yml
-ansible galera_container -m shell -a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'"
-openstack-ansible setup-openstack.yml
