e2c715e996ecaab4c77174ff2cc46638b03ab906
Olivier Bourdon <obourdon@opennext.io>
2018-04-27 18:47:45 +0200

    Add reject patterns so that dump doesn't grow exponentially
    Because ansible_local facts should be removed from stored list
    Adding Bifrost deployment step facts dump

--- ansible/playbooks/filter_plugins/hostvars_filter.py
+++ ansible/playbooks/filter_plugins/hostvars_filter.py
@@ -25,16 +25,23 @@ class FilterModule(object):
             'hostvars_filter': self.hostvars_filter
         }
 
-    def hostvars_filter(self, value, matching_keys = []):
+    def hostvars_filter(self, value, include_keys = [], exclude_keys = []):
         if not isinstance(value, string_types):
             raise AnsibleFilterError('Invalid value type (%s) for hostvars_filter (%s)' % (type(value), value))
-        if not isinstance(matching_keys, list):
-            raise AnsibleFilterError('Invalid keys type (%s) for hostvars_filter (%s)' % (type(matching_keys), matching_keys))
-        not_strings = filter(lambda x: not isinstance(x, string_types), matching_keys)
-        if len(not_strings) > 0:
-            raise AnsibleFilterError('Invalid keys type (not strings) for hostvars_filter items (%s)' % (not_strings))
-        if len(matching_keys) == 0:
-            raise AnsibleFilterError('Empty list of keys specified for hostvars_filter items')
+        if not isinstance(include_keys, list):
+            raise AnsibleFilterError('Invalid matching keys type (%s) for hostvars_filter (%s)' % (type(include_keys), include_keys))
+        if not isinstance(exclude_keys, list):
+            raise AnsibleFilterError('Invalid non matching keys type (%s) for hostvars_filter (%s)' % (type(exclude_keys), exclude_keys))
+        if len(include_keys) == 0 and len(exclude_keys) == 0:
+            raise AnsibleFilterError('List of matching and non matching keys can not be both empty for hostvars_filter')
+        if len(include_keys) > 0:
+            not_strings = filter(lambda x: not isinstance(x, string_types), include_keys)
+            if len(not_strings) > 0:
+                raise AnsibleFilterError('Invalid matching keys type (not strings) for hostvars_filter items (%s)' % (not_strings))
+        if len(exclude_keys) > 0:
+            not_strings = filter(lambda x: not isinstance(x, string_types), exclude_keys)
+            if len(not_strings) > 0:
+                raise AnsibleFilterError('Invalid non matching keys type (not strings) for hostvars_filter items (%s)' % (not_strings))
         ret = {}
         try:
             to_parse = json.loads(value)
@@ -43,10 +50,22 @@ class FilterModule(object):
                 lret = {}
                 # Level 2 is entries to be matched
                 for lk,lv in v.iteritems():
-                    for m in matching_keys:
+                    # Exclude has precedence over include
+                    to_exclude = False
+                    for m in exclude_keys:
                         if re.match(m, lk):
-                            lret[lk] = lv
-                            continue
+                            to_exclude = True
+                            break
+                    # Go on with next key, this one is excluded
+                    if to_exclude:
+                        continue
+                    if len(include_keys):
+                        for m in include_keys:
+                            if re.match(m, lk):
+                                lret[lk] = lv
+                                break
+                    else:
+                        lret[lk] = lv
                 if len(lret.keys()) > 0:
                     ret[k] = lret
         except Exception as e:
