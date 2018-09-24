51e7aefdae918a124146749dff70dfb3021ff432
Olivier Bourdon <obourdon@opennext.io>
2018-09-24 10:57:34 +0200

    Add some more checks for OSA deployment

--- ansible/files/osa_run_playbooks.sh
+++ ansible/files/osa_run_playbooks.sh
@@ -9,12 +9,20 @@ exec > >(tee -i /var/log/"$(basename "$0" .sh)"_"$(date '+%Y-%m-%d_%H-%M-%S')".l
 cd /opt/openstack-ansible/playbooks
 # Run host setup
 openstack-ansible setup-hosts.yml
+# In queens some playbooks have been added for extra checks
+if [ -r healthcheck-hosts.yml ]; then
+	openstack-ansible healthcheck-hosts.yml
+fi
 # Run YAML config files syntax checks
 openstack-ansible setup-infrastructure.yml --syntax-check
 # Run infra setup
 openstack-ansible setup-infrastructure.yml
 # Check Galera in infra
 ansible galera_container -m shell -a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'"
+# In queens some playbooks have been added for extra checks
+if [ -r healthcheck-infrastructure.yml ]; then
+	openstack-ansible healthcheck-infrastructure.yml
+fi
 # Run OpenStack setup
 openstack-ansible setup-openstack.yml
 
